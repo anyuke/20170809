875be769a097f2c8a23685f9b30b779ecbacc332
anyuke <1249836965@qq.com>
2017-08-10 19:21:04 +0800

    debug

--- weixin/app.js
+++ weixin/app.js
@@ -22,7 +22,10 @@ var api = new WechatAPI(weixinConfig.appid, weixinConfig.appsecret, function(cal
 }, function(token, callback) {
 	// 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
 	// 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+	token = JSON.parse(token);
 	console.log('token: \n', token.AccessToken.accessToken);
+	console.log('token.AccessToken: ', token.AccessToken);
+	console.log('token.AccessToken.accessToken: ', token.AccessToken.accessToken);
 	var sql = 'REPLACE INTO access_token(access_token) VALUES(?)';
 	var fields = [token.AccessToken.accessToken];
 	mysqlUtil.execute(sql, fields, function(err, result) {
