b0158d65a976c6c0f7ebc00761ddf34ac8493284
anyuke <1249836965@qq.com>
2017-08-11 17:42:22 +0800

    定时任务

--- weixin/task/weixin.js
+++ weixin/task/weixin.js
@@ -5,7 +5,7 @@ var weixinConfig = require('../config/weixin');
 
 exports.refresh= function () {
     var mission = function () {
-        request.post('https://api.weixin.qq.com/cgi-bin/token', {form: {grant_type: 'client_credential', appid: weixinConfig.appId, secret: weixinConfig.appSecret}}, function (err, rsp, body) {
+        request.post('https://api.weixin.qq.com/cgi-bin/token', {form: {grant_type: 'client_credential', appid: weixinConfig.appid, secret: weixinConfig.appsecret}}, function (err, rsp, body) {
             if (err) {
                 console.error(err);
                 return;
