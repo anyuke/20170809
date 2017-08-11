333e870aae97d5ff9a5b19fad9c0fc6ff350f770
anyuke <1249836965@qq.com>
2017-08-11 14:01:31 +0800

    fix

--- weixin/modules/wechat.js
+++ weixin/modules/wechat.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var wechat = require('wechat');
+var sha1 = require('sha1');
 
 //构建 WeChat 对象 即 js中 函数就是对象
 var WeChat = function(config) {
@@ -45,13 +46,33 @@ function wechatText(message, req, res, next) {
 }
 
 WeChat.prototype.auth = function(req, res) {
-	console.log('-----------------step 2');
-	var verifyInfo = { //验证信息
-		token: this.token,
-		appid: this.config.appid
-	};
-	console.log('verifyInfo:\n', verifyInfo);
-	wechat(verifyInfo, wechat.text(wechatText));
+	//1.获取微信服务器Get请求的参数 signature、timestamp、nonce、echostr
+	var signature = req.query.signature, //微信加密签名
+		timestamp = req.query.timestamp, //时间戳
+		nonce = req.query.nonce, //随机数
+		echostr = req.query.echostr; //随机字符串
+
+	//2.将token、timestamp、nonce三个参数进行字典序排序
+	var array = [this.token, timestamp, nonce];
+	array.sort();
+
+	//3.将三个参数字符串拼接成一个字符串进行sha1加密
+	var tempStr = array.join('');
+	// const hashCode = crypto.createHash('sha1'); //创建加密类型 
+	var resultCode = sha1(tempStr); //对传入的字符串进行加密
+
+	//4.开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+	if (resultCode === signature) {
+		res.send(echostr);
+	} else {
+		res.send('mismatch');
+	}
+	// var verifyInfo = { //验证信息
+	// 	token: this.token,
+	// 	appid: this.config.appid
+	// };
+	// console.log('verifyInfo:\n', verifyInfo);
+	// wechat(verifyInfo, wechat.text(wechatText));
 };
 
 //暴露可供外部访问的接口
